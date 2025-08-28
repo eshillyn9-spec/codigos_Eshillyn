47c9d4363b450be75c59c0092b3957afd6511f8b
Eshillyn Cristina <eshillyn9@gmail.com>
2025-08-28 15:48:41 -0300

    subindo codigos

---
+++ codigo Eshillyn/meu segundo programa.py
@@ -0,0 +1,19 @@
+#print("Eshillyn"*80000)
+#Exemplo de variavel variavel do tipo texto
+frutas ="manga"
+print(type(frutas))
+#variavel do tipo int e inteiro numeros inteiros
+idade = int(35)
+#exemlo de numero quebrado float
+nota =float(9,5)
+#booleana
+vc_curte=False
+#exemplo de variavel do tipo texto
+usuario ="Eshillyn"
+#exemplo de numero quebrado float
+valor =float(12,90)
+#variavel do tipo int e inteiro numeros inteiros
+idade =int(18)
+#Exemplo de variavel variavel do tipo texto
+musica ="jazz"
+print(type(jazz))
